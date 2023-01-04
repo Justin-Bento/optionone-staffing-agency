89c20382292a6ad83c132032b87da44b07a6eda7
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-01-04 17:20:06 -0400

    removed old layout and included a hero page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,55 +1,56 @@
 import Head from "next/head";
+import Image from "next/image";
 
-export default function Home() {
-  let company = "option one staffing agency";
+export default function index() {
   return (
     <>
       <Head>
         <title>Option One Staffing Agency</title>
       </Head>
-      <main className="mt-16 space-y-10">
-        <section className="p-4 space-y-8 text-start lg:text-center md:container md:mx-auto">
-          <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl lg:leading-tight">We Invest In Your Portential, <span className="md:block">Choosing Us Should Be Option One!</span></h1>
-          <p className="mt-4 text-gray-600 md:text-lg max-w-prose lg:mx-auto">Start your journey with {company}. Hand-picked professionals and expertly crafted components, designed for any kind of entrepreneur.</p>
-          <div className="flex flex-col gap-4 mt-4 lg:flex-row lg:justify-center">
-            <button className="inline-flex items-center justify-center w-auto px-3 py-2 space-x-2 text-sm font-medium text-white transition bg-blue-700 border border-blue-700 rounded appearance-none cursor-pointer select-none shrink-0 hover:border-blue-800 hover:bg-blue-800 focus:border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-300">
-              Sign Up Today
+      <main className="p-4 py-20 space-y-20 md:p-6 lg:p-8">
+        <section className="max-w-5xl mx-auto mt-12">
+          <div className="hidden sm:mb-4 sm:flex sm:justify-center">
+            <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+              <span className="text-gray-600">
+                View Listen To Our Success Stories.{' '}
+                <a href="#" className="font-semibold text-indigo-600">
+                  <span className="absolute inset-0" aria-hidden="true" />
+                  Read more <span aria-hidden="true">&rarr;</span>
+                </a>
+              </span>
+            </div>
+          </div>
+          <h1 className="max-w-3xl leading-tight type-display-medium md:text-center text-start md:mx-auto">
+            We Invest In Your Potential, Choosing Us Should Be Option One.
+          </h1>
+          <p className="max-w-3xl mx-auto mt-4 type-body-large text-start md:text-center">
+            Our dedicated staff is devoted to providing you the best possible
+            service and in doing so, we make sure that every step of the process
+            is handled in a professional and timely manner. Our team of
+            experienced and knowledgeable staff are here to assist you in
+            finding the perfect employee for your needs.
+          </p>
+          <div className="flex flex-col justify-start gap-4 mt-8 md:flex-row md:justify-center">
+            <button
+              type="button"
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+            >
+              Button text
             </button>
-            <button className="inline-flex items-center justify-center w-auto px-3 py-2 space-x-2 text-sm font-medium text-gray-800 transition bg-white border border-gray-200 rounded appearance-none cursor-pointer select-none shrink-0 hover:border-gray-300 hover:bg-gray-100 focus:border-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-300">
-              View Programs
+            <button
+              type="button"
+              className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Button text
             </button>
           </div>
-          <div className="bg-gray-200 w-full h-[720px] rounded-xl"></div>
-        </section>
-        <section className="bg-slate-600 text-slate-50">
-          <div className="px-4 py-16 md:container md:mx-auto">
-            <h2 className="text-3xl font-semibold capitalize">Call To Action</h2>
-            <p className=""></p>
-          </div>
-        </section>
-        <section className="px-4 py-16 md:container md:mx-auto">
-          <h3 className="text-3xl font-semibold capitalize">Headline 3</h3>
-          <p className=""></p>
-        </section>
-        <section className="px-4 py-16 md:container md:mx-auto">
-          <h4 className="text-3xl font-semibold capitalize">Headline 4</h4>
-          <p className=""></p>
-        </section>
-        <section className="bg-slate-600 text-slate-50">
-          <div className="px-4 py-16 md:container md:mx-auto">
-            <h5 className="text-3xl font-semibold capitalize">Call To Action</h5>
-            <p className=""></p>
-          </div>
-        </section>
-        <section className="px-4 py-16 md:container md:mx-auto">
-          <h6 className="text-3xl font-semibold capitalize">Headline 6</h6>
-          <p className=""></p>
-        </section>
-        <section className="bg-slate-600 text-slate-50">
-          <div className="px-4 py-16 md:container md:mx-auto">
-            <h2 className="text-3xl font-semibold capitalize">Call To Action</h2>
-            <p className=""></p>
-          </div>
+          <Image
+            src="/media/support-services.jpg"
+            alt="Image Of Company"
+            width={640}
+            height={600}
+            className="mt-8 md:w-full rounded-xl"
+          />
         </section>
       </main>
     </>

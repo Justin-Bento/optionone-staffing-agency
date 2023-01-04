d9f37695316879af7b7a0b8bbaa78c4d430f8b4a
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-01-04 17:19:50 -0400

    included global meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Appbar from '../components/Appbar'
-import Footer from '../components/Footer'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Appbar from "../components/Appbar";
+import Footer from "../components/Footer";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Option One Staffing Agency</title>
+        <meta name="title" content="Option One Staffing Agency" />
+        <meta name="description" content="Providing specialized services by supporting agencies and organizations that work with individuals experiencing behavioral, complex needs social difficulties." />
+      </Head>
       <Appbar />
       <Component {...pageProps} />
       <Footer />
     </>
-  )
+  );
 }

e68ccc766e99dcab6abfb67b647c201003b5daa9
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-01-04 18:42:08 -0400

    removed old layout and included a hero page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { HiOutlineBookOpen, HiOutlineCamera, HiOutlineCubeTransparent } from "react-icons/hi";
 
 export default function about() {
   return (
@@ -10,28 +11,83 @@ export default function about() {
         <section className="py-16 bg-slate-700 text-slate-50">
           <div className="p-4 md:container md:mx-auto max-w-7xl">
             <h1 className="font-serif text-lg tracking-wider">Our Mission</h1>
-            <p className="max-w-5xl text-xl italic tracking-wide">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, et eligendi, ut doloribus eum beatae, nihil quas quo quibusdam officiis obcaecati labore esse cupiditate veniam. Molestiae asperiores quam veritatis earum!</p>
+            <p className="max-w-5xl text-xl italic tracking-wide">
+              Our goal is to provide you with the best-customized support in
+              this rapidly growing economy. While you strive for excellence with
+              aid from our qualified staff who are devoted to providing you the
+              best possible service.
+            </p>
           </div>
         </section>
         <section className="p-4 md:container md:mx-auto max-w-7xl">
-          <h2 className="">Headline 2</h2>
-          <p className=""> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, et eligendi, ut doloribus eum beatae, nihil quas quo quibusdam officiis obcaecati labore esse cupiditate veniam. Molestiae asperiores quam veritatis earum!</p>
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+            <div className="1">
+              <HiOutlineBookOpen size={"2rem"} className="text-blue-700" />
+              <h2 className="mt-4 type-headline-small">Honesty</h2>
+              <p className="type-body-large">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint,
+                et eligendi, ut doloribus eum beatae, nihil quas quo quibusdam
+                officiis obcaecati labore esse cupiditate veniam. Molestiae
+                asperiores quam!
+              </p>
+            </div>
+            <div className="1">
+              <HiOutlineCamera size={"2rem"} className="text-blue-700" />
+              <h2 className="mt-4 type-headline-small">Excellence</h2>
+              <p className="type-body-large">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint,
+                et eligendi, ut doloribus eum beatae, nihil quas quo quibusdam
+                officiis obcaecati labore esse cupiditate veniam. Molestiae
+                asperiores quam!
+              </p>
+            </div>
+            <div className="1">
+              <HiOutlineCubeTransparent size={"2rem"} className="text-blue-700" />
+              <h2 className="mt-4 type-headline-small">Quality of service</h2>
+              <p className="type-body-large">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint,
+                et eligendi, ut doloribus eum beatae, nihil quas quo quibusdam
+                officiis obcaecati labore esse cupiditate veniam. Molestiae
+                asperiores quam!
+              </p>
+            </div>
+          </div>
         </section>
         <section className="p-4 md:container md:mx-auto max-w-7xl">
-          <h3 className="">Headline 3</h3>
-          <p className=""> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, et eligendi, ut doloribus eum beatae, nihil quas quo quibusdam officiis obcaecati labore esse cupiditate veniam. Molestiae asperiores quam veritatis earum!</p>
+          <h3 className="type-headline-medium">Headline 3</h3>
+          <p className="type-body-large">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, et
+            eligendi, ut doloribus eum beatae, nihil quas quo quibusdam officiis
+            obcaecati labore esse cupiditate veniam. Molestiae asperiores quam
+            veritatis earum!
+          </p>
         </section>
         <section className="p-4 md:container md:mx-auto max-w-7xl">
-          <h4 className="">Headline 4</h4>
-          <p className=""> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, et eligendi, ut doloribus eum beatae, nihil quas quo quibusdam officiis obcaecati labore esse cupiditate veniam. Molestiae asperiores quam veritatis earum!</p>
+          <h4 className="type-headline-medium">Headline 4</h4>
+          <p className="type-body-large">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, et
+            eligendi, ut doloribus eum beatae, nihil quas quo quibusdam officiis
+            obcaecati labore esse cupiditate veniam. Molestiae asperiores quam
+            veritatis earum!
+          </p>
         </section>
         <section className="p-4 md:container md:mx-auto max-w-7xl">
-          <h5 className="">Headline 5</h5>
-          <p className=""> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, et eligendi, ut doloribus eum beatae, nihil quas quo quibusdam officiis obcaecati labore esse cupiditate veniam. Molestiae asperiores quam veritatis earum!</p>
+          <h5 className="type-headline-medium">Headline 5</h5>
+          <p className="type-body-large">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, et
+            eligendi, ut doloribus eum beatae, nihil quas quo quibusdam officiis
+            obcaecati labore esse cupiditate veniam. Molestiae asperiores quam
+            veritatis earum!
+          </p>
         </section>
         <section className="p-4 md:container md:mx-auto max-w-7xl">
-          <h6 className="">Headline 6</h6>
-          <p className=""> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, et eligendi, ut doloribus eum beatae, nihil quas quo quibusdam officiis obcaecati labore esse cupiditate veniam. Molestiae asperiores quam veritatis earum!</p>
+          <h6 className="type-headline-medium">Headline 6</h6>
+          <p className="type-body-large">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, et
+            eligendi, ut doloribus eum beatae, nihil quas quo quibusdam officiis
+            obcaecati labore esse cupiditate veniam. Molestiae asperiores quam
+            veritatis earum!
+          </p>
         </section>
       </main>
     </>

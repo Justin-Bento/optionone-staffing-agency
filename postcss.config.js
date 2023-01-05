eebcc6842345dd42cf35911ca6efdb3703d73e06
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-01-04 22:20:17 -0400

    only allowed postcss to run in pages and components

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {
+      content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+      ],
+    },
     autoprefixer: {},
   },
 }

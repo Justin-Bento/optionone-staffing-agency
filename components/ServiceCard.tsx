a0d3370f63197c835f576df9a7ce886dd66ab913
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-01-04 22:21:57 -0400

    creaed card for services

--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -8,15 +8,11 @@ export default function ServiceCard(props: ServiceCardInterface) {
     <Link href={`/services/${props.Path}`} className="relative w-full h-64 rounded-lg">
       <img src={props.Image} alt="" className="absolute z-0 object-cover w-full h-64 rounded-lg" />
       <div className="absolute z-0 w-full h-64 transition-all rounded-lg bg-gray-900/70 hover:bg-gray-900/60 active:bg-gray-900/60"></div>
-      <div className="text-gray-50">
-        <div className="absolute bottom-0 left-0 p-8">
-        <h2 className="text-xl font-bold capitalize">{props.Headline}</h2>
-        <p className="">{props.Supporting}</p>
-        <button className="inline-flex gap-2">
-          Learn More
-          <HiChevronRight />
-        </button>
-      </div>
+      <div className="">
+        <div className="absolute bottom-0 left-0 p-8 ">
+          <h2 className="text-gray-50 type-title-large">{props.Headline}</h2>
+          <p className="max-w-3xl text-gray-50 type-body-medium">{props.Supporting}</p>
+        </div>
       </div>
     </Link>
   );

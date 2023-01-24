61035ac664ba5dfa0be2a89547c7688fc9fb80c4
Justin Bento <bento.a.justin@gmail.com>
2023-01-24 17:56:39 -0500

    installed tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: "jit",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Sofia Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
   ],
 }
